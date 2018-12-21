f8b62d4a77f9cabc84f98a97a7f3a860b84cfc13
rosi8814 <rahman_rofi@hotmail.com>
2018-12-21 13:51:02 +0100

    one file deleted

--- test_for_learning_git.c
+++ test_for_learning_git.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(){
 	printf("1st test of learning git. \n");
-	printf("Hello World. I am Rofi. \n");
+	printf("Hello World. I am Rofi and a students of engineering. \n");
 }
